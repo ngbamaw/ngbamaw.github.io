828a584c26565737d4c6c0602245d8b1ac5dfdf3
ngbamaw <williamliya@hotmail.fr>
2022-06-02 17:04:09 +0200

    fix errors expenses-chart

--- front-end-montor/expenses-chart-component-main/src/index.tsx
+++ front-end-montor/expenses-chart-component-main/src/index.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
